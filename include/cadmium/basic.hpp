b3736ef29ee87ef8056bc5a96d5d868827e42527
Román Cárdenas <rcardenas.rod@gmail.com>
2021-11-09 14:18:48 -0500

    Added str method to basic DEVS models

--- include/cadmium/basic.hpp
+++ include/cadmium/basic.hpp
@@ -18,24 +18,54 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef CADMIUM_BASIC_HPP_
-#define CADMIUM_BASIC_HPP_
+#ifndef _CADMIUM_BASIC_HPP_
+#define _CADMIUM_BASIC_HPP_
 
+#include <iostream>
+#include <memory>
+#include <utility>
 #include "core/modeling/atomic.hpp"
 #include "core/modeling/port.hpp"
 
 namespace cadmium {
 	struct Message {
 		virtual ~Message() = default;
+		virtual const std::string str() const = 0;
 	};
 
-	class BasicPort : public Port<Message> {};
+ 	class BasicPort : public Port<std::unique_ptr<Message>> {
+	  public:
+		template <typename T>
+		void addMessage(const T message) {
+			Port<std::unique_ptr<Message>>::addMessage(std::make_unique<T>(std::move(message)));
+		}
+
+		template <typename T>
+		static void addMessage(const std::shared_ptr<PortInterface>& port, const T message) {
+			Port<std::unique_ptr<Message>>::addMessage(port, std::make_unique<T>(std::move(message)));
+		}
+	};
+
+	std::ostream& operator<<(std::ostream &out, const std::unique_ptr<Message>& m) {
+		out << m->str();
+		return out;
+	}
 
 	struct State {
-		virtual ~Message() = default;
+		virtual ~State() = default;
+		virtual const std::string str() const = 0;
 	};
 
-	class BasicAtomic: public Atomic<State> {};
+ 	class BasicAtomic: public Atomic<std::unique_ptr<State>> {
+	 public:
+		template <typename S>
+		BasicAtomic(S initialState) : Atomic<std::unique_ptr<State>>(std::make_unique<S>(std::move(initialState))) {}
+	};
+
+	std::ostream& operator<<(std::ostream &out, const std::unique_ptr<State>& s) {
+		out << s->str();
+		return out;
+	}
 }
 
-#endif //CADMIUM_BASIC_HPP_
+#endif //_CADMIUM_BASIC_HPP_
