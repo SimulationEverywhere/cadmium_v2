90e63462f10f0ad0fe7f5abf4b467910f9835dd2
Srijan1972 <srijang@live.com>
2023-07-06 13:51:34 -0400

    Using asm delay

--- include/cadmium/simulation/rt_clock/ti_clock.hpp
+++ include/cadmium/simulation/rt_clock/ti_clock.hpp
@@ -23,12 +23,13 @@
 #define SEC_TO_MICRO 1000000.0
 #define MILI_TO_MICRO 1000.0
 
+#include <limits>
 #include "rt_clock.hpp"
 #include "../../exception.hpp"
 #include <timer_a.h>
-#include <timer32.h>
+// #include <timer32.h>
 #include <cs.h>
-#include <interrupt.h>
+// #include <interrupt.h>
 #include <rom.h>
 
 static volatile bool expired;
@@ -37,38 +38,41 @@ static volatile long numSeconds = 0;
 // * TimerA used to measure time between waitUntil calls
 // * Timer32 used to measure time for system to wait for next event
 
+extern "C" {
+    // void T32_INT1_IRQHandler(void){
+    //     ROM_Timer32_clearInterruptFlag (TIMER32_0_BASE); // reset interrupt flag
+    //     expired = true;
+    // }
+    void delay(long count){
+        __asm (
+            "pdloop: subs r0, #1\n"
+            "        bne pdloop\n");
+    }
+
+    void TA3_0_IRQHandler(void){
+        ROM_Timer_A_clearCaptureCompareInterrupt(TIMER_A3_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_0); // reset interrupt flag
+        numSeconds++;
+    }
+}
+
 const Timer_A_UpModeConfig upConfig =
 {
     TIMER_A_CLOCKSOURCE_SMCLK,
     TIMER_A_CLOCKSOURCE_DIVIDER_1,
-    31999,
+    32000,
     TIMER_A_TAIE_INTERRUPT_DISABLE,
     TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE,
     TIMER_A_DO_CLEAR
 };
 
-void T32_INT1_IRQHandler(void)
-{
-    ROM_Timer32_clearInterruptFlag (TIMER32_0_BASE); // reset interrupt flag
-    expired = true;
-}
-
-void TA3_0_IRQHandler(void)
-{
-    ROM_Timer_A_clearCaptureCompareInterrupt(TIMER_A3_BASE,TIMER_A_CAPTURECOMPARE_REGISTER_0); // reset interrupt flag
-    numSeconds++;
-}
-
 namespace cadmium {
     /**
      * Real-time clock based on the Driver APIs provided by TI for MSP432P401R
-     * @tparam T Internal clock type. By default, it uses a double.
      */
-    template <typename T = double>
     class TIClock : RealTimeClock {
     private:
     protected:
-        T rTimeLast;
+        double rTimeLast;
         long maxJitter; //! Maximum allowed delay jitter. This parameter is optional.
     public:
         volatile bool interrupted;
@@ -81,10 +85,9 @@ namespace cadmium {
             rTimeLast = 0.0;
             ROM_CS_setReferenceOscillatorFrequency(CS_REFO_32KHZ);
             ROM_CS_initClockSignal(CS_MCLK,CS_REFOCLK_SELECT,CS_CLOCK_DIVIDER_1);
-            ROM_Timer32_initModule(TIMER32_0_BASE,TIMER32_PRESCALER_1,TIMER32_32BIT,TIMER32_PERIODIC_MODE);
-            ROM_Interrupt_enableSleepOnIsrExit();
-            ROM_Interrupt_enableInterrupt(TIMER32_0_INTERRUPT);
-            ROM_Timer32_enableInterrupt(TIMER32_0_BASE);
+            // ROM_Timer32_initModule(TIMER32_0_BASE,TIMER32_PRESCALER_1,TIMER32_32BIT,TIMER32_PERIODIC_MODE);
+            // ROM_Interrupt_enableInterrupt(TIMER32_0_INTERRUPT);
+            // ROM_Timer32_enableInterrupt(TIMER32_0_BASE);
             ROM_CS_initClockSignal(CS_SMCLK,CS_REFOCLK_SELECT,CS_CLOCK_DIVIDER_1);
             ROM_Timer_A_configureUpMode(TIMER_A3_BASE,&upConfig);
             ROM_Interrupt_enableInterrupt(INT_TA3_0);
@@ -123,29 +126,56 @@ namespace cadmium {
          * Waits until the next simulation time.
          * @param nextTime next simulation time (in seconds).
          */
-        T waitUntil(double timeNext) override {
+        double waitUntil(double timeNext) override {
             ROM_Timer_A_stopTimer(TIMER_A3_BASE);
-            long expected = (timeNext-vTimeLast == std::numeric_limits<double>::infinity())?std::numeric_limits<long>::max():((timeNext-vTimeLast-numSeconds)*SEC_TO_MICRO + ROM_Timer_A_getCounterValue(TIMER_A3_BASE)*MILI_TO_MICRO/32);
-            long actual = 0;
+            // long expected = (timeNext-vTimeLast == std::numeric_limits<double>::infinity())?std::numeric_limits<long>::max():((timeNext-vTimeLast-numSeconds)*SEC_TO_MICRO + ROM_Timer_A_getCounterValue(TIMER_A3_BASE)*MILI_TO_MICRO/32);
+            // long actual = 0;
+            // expired = false;
+            // numSeconds = 0;
+            // ROM_Timer_A_startCounter(TIMER_A3_BASE,TIMER_A_UP_MODE);
+            // long left = expected;
+            // while(!interrupted && left > 0){
+            //     expired = false;
+            //     long temp = std::min((long)SEC_TO_MICRO,left);
+            //     ROM_Timer32_setCount(TIMER32_0_BASE,temp*32/MILI_TO_MICRO);
+            //     ROM_Timer32_startTimer(TIMER32_0_BASE,true);
+            //     while(!expired && !interrupted) __sleep();
+            //     if(!interrupted) left-=temp;
+            // }
+            // expired = false;
+            // ROM_Timer_A_stopTimer(TIMER_A3_BASE);
+            // left -= (numSeconds*SEC_TO_MICRO+ROM_Timer_A_getCounterValue(TIMER_A3_BASE)*MILI_TO_MICRO/32);
+            // actual = std::max(expected-left,0l);
+            // double duration = actual/SEC_TO_MICRO;
+            // rTimeLast += duration;
+            // return RealTimeClock::waitUntil(vTimeLast + duration);
+            long expected = (timeNext-vTimeLast-numSeconds)*MILI_TO_MICRO-ROM_Timer_A_getCounterValue(TIMER_A3_BASE)/32; // milliseconds
+            if(maxJitter >= 0){
+                auto jitter = std::abs(std::min(expected,0l));
+                if(jitter > maxJitter){
+                    throw cadmium::CadmiumRTClockException("delay jitter is too high");
+                }
+            }
             expired = false;
             numSeconds = 0;
             ROM_Timer_A_startCounter(TIMER_A3_BASE,TIMER_A_UP_MODE);
             long left = expected;
             while(!interrupted && left > 0){
                 expired = false;
-                long temp = std::min((long)SEC_TO_MICRO,left);
-                ROM_Timer32_setCount(TIMER32_0_BASE,3*temp-1);
-                ROM_Timer32_startTimer(TIMER32_0_BASE,true);
-                while(!expired && !interrupted) __sleep();
-                if(!interrupted) left-=temp;
+                long temp = std::min((long)MILI_TO_MICRO,left);
+                long res = temp;
+                while(!interrupted && temp > 0){
+                    delay(8000);
+                    temp--;
+                }
+                if(!interrupted) left-=res;
             }
             expired = false;
             ROM_Timer_A_stopTimer(TIMER_A3_BASE);
-            left -= (numSeconds*SEC_TO_MICRO+ROM_Timer_A_getCounterValue(TIMER_A3_BASE)*MILI_TO_MICRO/32);
-            actual = std::max(expected-left,0l);
-            double duration = actual*SEC_TO_MICRO;
+            left -= ((numSeconds+ROM_Timer_A_getCounterValue(TIMER_A3_BASE)/32)*MILI_TO_MICRO);
+            double duration = std::max(expected-left,0l)/MILI_TO_MICRO;
             rTimeLast += duration;
-            return RealTimeClock::waitUntil(vTimeLast + duration);
+            return RealTimeClock::waitUntil(vTimeLast+duration);
         }
 
         void update(){
